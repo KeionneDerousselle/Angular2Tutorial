110b20f1c94bb5610ac810c4f7678b4542288dc6
Keionne Derousselle <Keionne.Derousselle@extendhealth.com>
2017-05-10 16:46:33 -0600

    using http service in a service

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -15,14 +15,19 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number =2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+    listFilter: string = '';
+    errorMessage: string;
     products: IProduct[] =[];
 
     constructor(private _productService: ProductService){
     }
 
     ngOnInit(): void{
-       this.products = this._productService.getProducts();
+       
+        this._productService.getProducts()
+            .subscribe(
+                products => this.products = products,
+                error => this.errorMessage = <any>error);
     }
 
     toggleImage(): void {
