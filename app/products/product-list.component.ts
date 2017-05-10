e042b9899628ab9a0f6fdb4e1f2f76fed065ef32
Keionne Derousselle <Keionne.Derousselle@extendhealth.com>
2017-05-10 14:36:40 -0600

    component output data via events

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -55,4 +55,8 @@ export class ProductListComponent implements OnInit{
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Our Products: ' + message;
+    }
 }
